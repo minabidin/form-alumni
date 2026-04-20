2b56f944b36dd1da0f9c176b46480951295844e5
minabidin <121146648+minabidin@users.noreply.github.com>
2026-04-20 11:58:50 +0700

    Refactor script.js to streamline region selection

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const API_URL = "URL_WEB_APP_ANDA";
 
-const prov = document.getElementById("provinsi");
 const kab = document.getElementById("kabupaten");
 const kec = document.getElementById("kecamatan");
 const desa = document.getElementById("desa");
@@ -13,28 +12,17 @@ fetch(API_URL)
   .then(data => {
     dataWilayah = data;
 
-    Object.keys(dataWilayah).forEach(p => {
-      prov.innerHTML += `<option value="${p}">${p}</option>`;
+    Object.keys(dataWilayah).forEach(k => {
+      kab.innerHTML += `<option value="${k}">${k}</option>`;
     });
   });
-
-// PROVINSI → KABUPATEN
-prov.onchange = () => {
-  kab.innerHTML = '<option value="">Pilih Kabupaten</option>';
-  kec.innerHTML = '<option value="">Pilih Kecamatan</option>';
-  desa.innerHTML = '<option value="">Pilih Desa</option>';
-
-  Object.keys(dataWilayah[prov.value] || {}).forEach(k => {
-    kab.innerHTML += `<option value="${k}">${k}</option>`;
-  });
-};
 
 // KABUPATEN → KECAMATAN
 kab.onchange = () => {
   kec.innerHTML = '<option value="">Pilih Kecamatan</option>';
   desa.innerHTML = '<option value="">Pilih Desa</option>';
 
-  Object.keys(dataWilayah[prov.value][kab.value] || {}).forEach(k => {
+  Object.keys(dataWilayah[kab.value] || {}).forEach(k => {
     kec.innerHTML += `<option value="${k}">${k}</option>`;
   });
 };
@@ -43,28 +31,7 @@ kab.onchange = () => {
 kec.onchange = () => {
   desa.innerHTML = '<option value="">Pilih Desa</option>';
 
-  (dataWilayah[prov.value][kab.value][kec.value] || []).forEach(d => {
+  (dataWilayah[kab.value][kec.value] || []).forEach(d => {
     desa.innerHTML += `<option value="${d}">${d}</option>`;
-  });
-};
-
-// TAMAT LOGIC
-document.getElementById("tamat").onchange = function () {
-  document.getElementById("tahunTamat").classList.toggle("d-none", this.value !== "Ya");
-};
-
-// SUBMIT
-document.getElementById("formData").onsubmit = function(e) {
-  e.preventDefault();
-
-  fetch(API_URL, {
-    method: "POST",
-    body: new FormData(this)
-  })
-  .then(res => res.text())
-  .then(() => {
-    document.getElementById("status").innerHTML =
-      "<div class='alert alert-success'>Data berhasil dikirim</div>";
-    this.reset();
   });
 };
